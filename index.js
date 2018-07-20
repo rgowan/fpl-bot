3a2c40f21ae7c8dd81788bed15dc68730da84085
rgowan <ranegowan@gmail.com>
2018-07-20 16:18:42 +0100

    removing try catch as it's not needed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const SlackBot = require('slackbots');
 const getRoundScores = require('./scripts/getRoundScores');
 const PORT = process.env.PORT || 3000;
-
 require('dotenv').config();
 
 const app = express();
@@ -13,23 +12,19 @@ const bot = new SlackBot({
 });
 
 async function createMessage(channel) {
-  try {
-    const players = await getRoundScores();
+  const players = await getRoundScores();
 
-    const messageArray = [
-      'Here are the top performers from last GW ⚽️️️️️ 🏆\n\n',
-      'Score | Name | Club | Price | Position | Form\n\n'
-    ];
+  const messageArray = [
+    'Here are the top performers from last GW ⚽️️️️️ 🏆\n\n',
+    'Score | Name | Club | Price | Position | Form\n\n'
+  ];
 
-    players.forEach((player) => {
-      const playerString = `${player.score}\t${player.name} ${player.club} ${player.cost} ${player.position} ${player.form}\n\n`;
-      messageArray.push(playerString);
-    });
+  players.forEach((player) => {
+    const playerString = `${player.score}\t${player.name} ${player.club} ${player.cost} ${player.position} ${player.form}\n\n`;
+    messageArray.push(playerString);
+  });
 
-    bot.postMessageToChannel('general', messageArray.join(''));
-  } catch (err) {
-    console.log('something went wrong');
-  }
+  bot.postMessageToChannel('general', messageArray.join(''));
 }
 
 createMessage();
